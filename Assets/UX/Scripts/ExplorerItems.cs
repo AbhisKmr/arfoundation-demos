50a89699d413f326d37ec01933542dac3fa71719
AbhisKmr <akaks09@gmail.com>
2022-05-30 11:32:43 +0530

    update: working on ios

---
+++ Assets/UX/Scripts/ExplorerItems.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExplorerItems : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bottomSheet;
+
+    [SerializeField]
+    private GameObject template;
+
+    [SerializeField]
+    private List<Sprite> img = new List<Sprite>();
+
+    private Vector3 initialSheetpos = Vector3.zero;
+    private Vector3 hideSheetPos = new Vector3(585.00f, -70.72f, 0.00f);
+    private float speed = 3f;
+    private PlaceObjectsOnPlane placeObjectsOnPlane;
+
+
+    void Start()
+    {
+        bottomSheet.SetActive(false);
+        placeObjectsOnPlane = transform.GetComponent<PlaceObjectsOnPlane>();
+        List<GameObject> itemToShow = placeObjectsOnPlane.placedPrefab;
+
+        for (int i = 0; i < itemToShow.Count; i++)
+        {
+            var t = Instantiate(template, bottomSheet.transform.transform);
+            t.name = i.ToString();
+            t.transform.GetChild(0).GetComponent<Image>().sprite = img[i];
+
+            t.GetComponent<Button>().onClick.AddListener(delegate
+            {
+                OnItemClicked(int.Parse(t.name));
+            });
+        }
+
+        //initialSheetpos = bottomSheet.transform.position;
+        //bottomSheet.transform.position = hideSheetPos;
+    }
+
+    public void explorerItems()
+    {
+        SheetState(bottomSheet.activeSelf);
+
+        Debug.Log("You have clicked the bottomsheet:// :)");
+    }
+
+    void OnItemClicked(int s)
+    {
+        placeObjectsOnPlane.readyForPlaceItem = true;
+        placeObjectsOnPlane.PlacementIntex = s;
+        bottomSheet.SetActive(false);
+        Debug.Log(s);
+    }
+
+    private void SheetState(bool b)
+    {
+        //bottomSheet.transform.position = b ? hideSheetPos: initialSheetpos;
+        placeObjectsOnPlane.BottomSheetActive = b;
+        bottomSheet.SetActive(!b);
+    }
+}
--- Assets/UX/Scripts/PlaceObjectsOnPlane.cs
+++ Assets/UX/Scripts/PlaceObjectsOnPlane.cs
@@ -7,27 +7,30 @@ using UnityEngine.XR.ARSubsystems;
 [RequireComponent(typeof(ARRaycastManager))]
 public class PlaceObjectsOnPlane : MonoBehaviour
 {
+
     [SerializeField]
     [Tooltip("Instantiates this prefab on a plane at the touch location.")]
-    GameObject m_PlacedPrefab;
+    List<GameObject> m_PlacedPrefab;
+    [SerializeField]
+    private GameObject bottomBar;
+    [SerializeField]
+    private GameObject placementLocator;
 
-    /// <summary>
-    /// The prefab to instantiate on touch.
-    /// </summary>
-    public GameObject placedPrefab
+    int placementIndex = 0;
+    public int PlacementIntex
+    {
+        get { return placementIndex; }
+        set { placementIndex = value; }
+    }
+
+    public List<GameObject> placedPrefab
     {
         get { return m_PlacedPrefab; }
         set { m_PlacedPrefab = value; }
     }
 
-    /// <summary>
-    /// The object instantiated as a result of a successful raycast intersection with a plane.
-    /// </summary>
     public GameObject spawnedObject { get; private set; }
 
-    /// <summary>
-    /// Invoked whenever an object is placed in on a plane.
-    /// </summary>
     public static event Action onPlacedObject;
 
     ARRaycastManager m_RaycastManager;
@@ -48,12 +51,51 @@ public class PlaceObjectsOnPlane : MonoBehaviour
         set => m_CanReposition = value;
     }
 
+    bool isBottomSheetActive = false;
+
+    public bool BottomSheetActive
+    {
+        get => isBottomSheetActive;
+        set => isBottomSheetActive = value;
+    }
+
+    bool isReadyForPlaceItem = false;
+    Vector3 touchPosWorld;
+    TouchPhase touchPhase = TouchPhase.Ended;
+
+    public bool readyForPlaceItem
+    {
+        get => isReadyForPlaceItem;
+        set => isReadyForPlaceItem = value;
+    }
+
     void Awake()
     {
         m_RaycastManager = GetComponent<ARRaycastManager>();
+        placementLocator.SetActive(false);
     }
 
     void Update()
+    {
+        bool isPlanDetected = m_RaycastManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), s_Hits, TrackableType.PlaneWithinPolygon);
+
+        if (isPlanDetected)
+        {
+            bottomBar.SetActive(true);
+
+            if (isReadyForPlaceItem)
+            {
+                Pose hitPose = s_Hits[0].pose;
+                placementLocator.transform.SetPositionAndRotation(hitPose.position, hitPose.rotation);
+                placementLocator.SetActive(true);
+
+                if (!isBottomSheetActive)
+                    TouchOnLocator(hitPose);
+            }
+        }
+    }
+
+    private void TouchOnLocator(Pose hitPose)
     {
         if (Input.touchCount > 0)
         {
@@ -63,22 +105,23 @@ public class PlaceObjectsOnPlane : MonoBehaviour
             {
                 if (m_RaycastManager.Raycast(touch.position, s_Hits, TrackableType.PlaneWithinPolygon))
                 {
-                    Pose hitPose = s_Hits[0].pose;
-
                     if (m_NumberOfPlacedObjects < m_MaxNumberOfObjectsToPlace)
                     {
-                        spawnedObject = Instantiate(m_PlacedPrefab, hitPose.position, hitPose.rotation);
-                        
+                        spawnedObject = Instantiate(m_PlacedPrefab[placementIndex], hitPose.position, hitPose.rotation);
+
                         m_NumberOfPlacedObjects++;
                     }
-                    else
-                    {
-                        if (m_CanReposition)
-                        {
-                            spawnedObject.transform.SetPositionAndRotation(hitPose.position, hitPose.rotation);
-                        }
-                    }
-                    
+
+                    isReadyForPlaceItem = false;
+                    placementLocator.SetActive(false);
+                    //else
+                    //{
+                    //    //if (m_CanReposition)
+                    //    //{
+                    //    //    spawnedObject.transform.SetPositionAndRotation(hitPose.position, hitPose.rotation);
+                    //    //}
+                    //}
+
                     if (onPlacedObject != null)
                     {
                         onPlacedObject();
@@ -87,4 +130,60 @@ public class PlaceObjectsOnPlane : MonoBehaviour
             }
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+     *  
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            placementIndex %= 3;
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (m_RaycastManager.Raycast(touch.position, s_Hits, TrackableType.PlaneWithinPolygon))
+                {
+                    Pose hitPose = s_Hits[0].pose;
+
+                    if (m_NumberOfPlacedObjects < m_MaxNumberOfObjectsToPlace)
+                    {
+                        spawnedObject = Instantiate(m_PlacedPrefab[placementIndex], hitPose.position, hitPose.rotation);
+
+                        m_NumberOfPlacedObjects++;
+                        placementIndex++;
+                    }
+                    else
+                    {
+                        if (m_CanReposition)
+                        {
+                            spawnedObject.transform.SetPositionAndRotation(hitPose.position, hitPose.rotation);
+                        }
+                    }
+
+                    if (onPlacedObject != null)
+                    {
+                        onPlacedObject();
+                    }
+                }
+            }
+        }
+     */
 }
