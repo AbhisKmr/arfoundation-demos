aaddf959ba7da608ed11803eaeea87bfd8b3c705
AbhisKmr <akaks09@gmail.com>
2022-06-03 09:12:30 +0530

    wordrobe animation

---
+++ Assets/Common/Scripts/AnimatorHandler.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class AnimatorHandler : MonoBehaviour
+{
+    [SerializeField]
+    private int animSize;
+
+    private List<string> StartString = new List<string>();
+    private string EndString = "EndAnim";
+    private string StartPrefix = "Step";
+
+    private int animIndex;
+
+    private Animator animator;
+    private bool isAnimFinished = false;
+
+    public bool clickedOnObject = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        foreach (var i in Enumerable.Range(0, animSize))
+        {
+            StartString.Add(StartPrefix + i);
+        }
+    }
+
+    void Update()
+    {
+        if (animator != null)
+        {
+            if (clickedOnObject)
+            {
+                if (animIndex == StartString.Count)
+                {
+                    isAnimFinished = true;
+                    Debug.Log("Run finigh animation");
+                    animator.SetTrigger(EndString);
+                }
+                if (!isAnimFinished)
+                {
+                    animIndex %= StartString.Count;
+                    animator.SetTrigger(StartString[animIndex]);
+                    animIndex++;
+                }
+                else
+                {
+                    animIndex %= StartString.Count;
+                    isAnimFinished = false;
+                }
+
+                clickedOnObject = false;
+            }
+        }
+    }
+}
--- Assets/Common/Scripts/TouchProductHelper.cs
+++ Assets/Common/Scripts/TouchProductHelper.cs
@@ -5,43 +5,134 @@ using UnityEngine;
 public class TouchProductHelper : MonoBehaviour
 {
     private PlaceObjectsOnPlane placeObjectsOnPlane;
+    Vector3 initialScale = Vector3.zero;
+    float initialDistance = 0f;
+    private AnimatorHandler animatorHandler;
+    bool lMove = false;
+
 
     private void Start()
     {
         placeObjectsOnPlane = FindObjectOfType<PlaceObjectsOnPlane>();
+
+        foreach (Transform t in transform)
+        {
+            animatorHandler = t.gameObject.GetComponent<AnimatorHandler>();
+            if (animatorHandler != null) break;
+        }
     }
 
     void Update()
     {
-        if (Input.touchCount > 0 &&
-            (Input.touches[0].phase == TouchPhase.Began || Input.touches[0].phase == TouchPhase.Moved))
+        if (Input.touchCount > 0)
         {
             placeObjectsOnPlane.TappedOnObject = true;
+            Touch touch = Input.GetTouch(0);
 
-            Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
-
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit))
+            switch (touch.phase)
             {
-                if (hit.collider.name == transform.name)
-                {
-                    if (Input.touchCount == 1)
+                case TouchPhase.Moved:
+                    Debug.Log("Touch Move");
+                    onDragFinger();
+                    lMove = true;
+                    break;
+
+                case TouchPhase.Began:
+                    Debug.Log("Touch Began");
+                    break;
+
+                case TouchPhase.Ended:
+                    Debug.Log("Touch ended");
+                    if (!lMove)
                     {
-                        Touch screenTouch = Input.GetTouch(0);
-                        if (screenTouch.phase == TouchPhase.Moved)
-                        {
-                            transform.Rotate(0f, -1 * screenTouch.deltaPosition.x, 0f);
-                            //foreach(Transform c in transform)
-                            //{
-                            //    c.gameObject.transform.Rotate(0f, -1 * screenTouch.deltaPosition.x, 0f);
-                            //}
-                        }
+                        onTouchObject();
                     }
-                }
+                    break;
             }
+            lMove = false;
         }
         else
             placeObjectsOnPlane.TappedOnObject = false;
+    }
+
+    private void onTouchObject()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.name == transform.name)
+            {
+                if (Input.touchCount == 1) 
+                {
+                    if (animatorHandler != null)
+                    {
+                        animatorHandler.clickedOnObject = true;
+                    }
+                    
+                }
+            }
+        }
+    }
+
+    private void onDragFinger()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.name == transform.name)
+            {
+                if (Input.touchCount == 1) // to rotate
+                {
+                    Touch screenTouch = Input.GetTouch(0);
+                    if (screenTouch.phase == TouchPhase.Moved)
+                    {
+                        transform.Rotate(0f, -1 * screenTouch.deltaPosition.x, 0f);
+                    }
+                }
+
+                else if (false) // to scale (Input.touchCount == 2)
+                {
+                    Touch zeroTouch = Input.GetTouch(0);
+                    Touch oneTouch = Input.GetTouch(1);
+
+                    if (zeroTouch.phase == TouchPhase.Ended || zeroTouch.phase == TouchPhase.Canceled ||
+                        oneTouch.phase == TouchPhase.Ended || oneTouch.phase == TouchPhase.Canceled)
+                    {
+                        Debug.Log("return");
+                        return;
+                    }
+
+                    var statmentOne = zeroTouch.phase == TouchPhase.Began || zeroTouch.phase == TouchPhase.Stationary;
+                    var statmentTwo = oneTouch.phase == TouchPhase.Began || oneTouch.phase == TouchPhase.Stationary;
+
+                    if (statmentOne || statmentTwo)
+                    {
+                        initialDistance = Vector2.Distance(zeroTouch.position, oneTouch.position);
+                        initialScale = transform.localScale;
+
+                        Debug.Log("dis: " + initialDistance + " :: scale\\" + initialScale);
+                    }
+                    else
+                    {
+                        Debug.Log("tap else");
+
+                        float currentDistance = Vector2.Distance(zeroTouch.position, oneTouch.position);
+
+                        if (Mathf.Approximately(initialDistance, 0)) return;
+
+                        var factor = currentDistance / initialDistance;
+                        Debug.Log("next:: " + currentDistance + "::" + factor);
+
+                        transform.localScale = initialScale * factor;
+                    }
+                } 
+            }
+        }
     }
 }
