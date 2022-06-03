a64636a416133306e7799485e9bfcf6a38529ec7
AbhisKmr <akaks09@gmail.com>
2022-06-03 18:51:38 +0530

    bottomsheet ui fixed

---
+++ Assets/Common/Scripts/BottomSheetHelper.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottomSheetHelper : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bottomSheet;
+
+    [SerializeField]
+    private GameObject sheetList;
+
+    private bool isSheetStateShow = true;
+    private Vector3 showPos = new Vector3();
+
+    private void Start()
+    {
+        showPos = bottomSheet.transform.position;
+        BottonSheetPosition();
+    }
+
+    public void BottonSheetPosition()
+    {
+        var hidePos = new Vector3(showPos.x, 245f, showPos.z);
+
+        bottomSheet.transform.position = isSheetStateShow ? showPos: hidePos;
+
+        sheetList.SetActive(isSheetStateShow);
+        
+        isSheetStateShow = !isSheetStateShow;
+    }
+}
